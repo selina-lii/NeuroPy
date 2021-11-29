2a17faa4187ceca8b8ccf7e41434c51d7a16c1d5
bapungiri <bapun.k.giri@gmail.com>
2021-11-29 12:32:06 -0500

    iso_distance and l_ratio for PhyIO data

--- neuropy/core/datawriter.py
+++ neuropy/core/datawriter.py
@@ -1,16 +1,17 @@
 import numpy as np
-import pathlib
 from pathlib import Path
 
+
 class DataWriter:
-    def __init__(self, metadata=None) -> None:
+    def __init__(self, metadata: dict = None) -> None:
 
         self._filename = None
 
         if metadata is not None:
             assert isinstance(metadata, dict), "Only dictionary accepted as metadata"
-
-        self._metadata: dict = metadata
+            self._metadata: dict = metadata
+        else:
+            self._metadata: dict = {}
 
     @property
     def filename(self):
@@ -36,72 +37,19 @@ class DataWriter:
     def from_dict(d):
         return NotImplementedError
 
-    # @staticmethod
-    # def from_file(f):
-    #     if f.is_file():
-    #         try:
-    #             d = np.load(f, allow_pickle=True).item()
-    #             return d
-    #         except NotImplementedError:
-    #             print("Issue with pickled POSIX_PATH on windows for path {}, falling back to non-pickled version...".format(f))
-    #             temp = pathlib.PosixPath
-    #             # pathlib.PosixPath = pathlib.WindowsPath # Bad hack
-    #             pathlib.PosixPath = pathlib.PurePosixPath # Bad hack
-    #             d = np.load(f, allow_pickle=True).item()
-    #             # d['filename']
-    #             # print("Post hack decode: {}\n".format(d))
-    #             return d
-            
-    #         return None
-    #     else:
-    #         return None
-    
-    # @staticmethod
-    # def from_file(f):
-    #     d = DataWriter.from_file(f)
-    #     if d is not None:
-    #         return Position.from_dict(d)
-    #     else:
-    #         return None
-
-
-    @classmethod
-    def from_file(cls, f):
+    @staticmethod
+    def from_file(f):
         if f.is_file():
-            dict_rep = None
-            try:
-                dict_rep = np.load(f, allow_pickle=True).item()
-                # return dict_rep
-            except NotImplementedError:
-                print("Issue with pickled POSIX_PATH on windows for path {}, falling back to non-pickled version...".format(f))
-                temp = pathlib.PosixPath
-                # pathlib.PosixPath = pathlib.WindowsPath # Bad hack
-                pathlib.PosixPath = pathlib.PurePosixPath # Bad hack
-                dict_rep = np.load(f, allow_pickle=True).item()
-                # d['filename']
-                # print("Post hack decode: {}\n".format(d))
-                # return dict_rep
-            
-            if dict_rep is not None:
-                # Convert to object
-                obj = cls.from_dict(dict_rep)
-                obj.filename = f
-                return obj
-            
-            
-            return dict_rep
-            
+            d = np.load(f, allow_pickle=True).item()
+            return d
         else:
             return None
-        
-        
 
-
-
-    def to_dict(self, recurrsively=False):
+    def to_dict(self):
         return NotImplementedError
 
     def save(self):
+
         data = self.to_dict()
         if self.filename is not None:
             assert isinstance(self.filename, Path)
