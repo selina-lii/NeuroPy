3e0d6b3543040ee889c9aa9601a0e06a05bf13b8
Pho Hale <PhoHale@gmail.com>
2021-12-21 17:50:16 -0500

    refactored to use new PfNd class

--- neuropy/plotting/ratemaps.py
+++ neuropy/plotting/ratemaps.py
@@ -1,20 +1,19 @@
+from ipywidgets import widgets
 import matplotlib.pyplot as plt
 import matplotlib as mpl
 import numpy as np
 from .. import core
-from ..utils import mathutil
+from neuropy.utils import mathutil
 from .figure import Fig
 
 
-def plot_ratemap(
-    ratemap: core.Ratemap,
+def plot_ratemap(ratemap: core.Ratemap,
     normalize_xbin=False,
     ax=None,
     pad=2,
     normalize_tuning_curve=False,
     sortby=None,
-    cmap="tab20b",
-):
+    cmap="tab20b"):
     """Plot 1D place fields stacked
 
     Parameters
@@ -59,8 +58,19 @@ def plot_ratemap(
         sort_ind = sortby
     else:
         sort_ind = np.arange(n_neurons)
+
+    ## TODO: actually sort the ratemap object's neuron_ids and tuning_curves by the sort_ind
+    # sorted_neuron_ids = ratemap.neuron_ids[sort_ind]
+    
+    sorted_neuron_ids = np.take_along_axis(np.array(ratemap.neuron_ids), sort_ind, axis=0)
+    # sorted_tuning_curves = tuning_curves[sorted_neuron_ids, :]
+    sorted_neuron_id_labels = ['Cell[{}]'.format(a_neuron_id) for a_neuron_id in sorted_neuron_ids]
+    
+    colors_array = np.zeros((4, n_neurons))
     for i, neuron_ind in enumerate(sort_ind):
         color = cmap(i / len(sort_ind))
+        colors_array[:, i] = color
+        curr_neuron_id = sorted_neuron_ids[i]
 
         ax.fill_between(
             bin_cntr,
@@ -68,19 +78,22 @@ def plot_ratemap(
             i * pad + tuning_curves[neuron_ind],
             color=color,
             ec=None,
-            alpha=0.7,
+            alpha=0.5,
             zorder=i + 1,
         )
         ax.plot(
             bin_cntr,
             i * pad + tuning_curves[neuron_ind],
             color=color,
-            alpha=1,
-            lw=0.6,
+            alpha=0.7,
         )
+        ax.set_title('Cell[{}]'.format(curr_neuron_id))
 
-    ax.set_yticks(list(range(len(sort_ind))))
-    ax.set_yticklabels(list(ratemap.neuron_ids[sort_ind]))
+    # ax.set_yticks(list(range(len(sort_ind)) + 0.5))
+    ax.set_yticks(list(np.arange(len(sort_ind)) + 0.5))
+    # ax.set_yticklabels(list(sort_ind))
+    ax.set_yticklabels(list(sorted_neuron_id_labels))
+    
     ax.set_xlabel("Position")
     ax.spines["left"].set_visible(False)
     if normalize_xbin:
@@ -90,10 +103,10 @@ def plot_ratemap(
     # if self.run_dir is not None:
     #     ax.set_title(self.run_dir.capitalize() + " Runs only")
 
-    return ax
+    return ax, sort_ind, colors_array
 
 
-def plot_raw(self, ax=None, subplots=(8, 9)):
+def plot_raw(ratemap: core.Ratemap, t, x, run_dir, ax=None, subplots=(8, 9)):
     """Plot spike location on animal's path
 
     Parameters
@@ -106,16 +119,17 @@ def plot_raw(self, ax=None, subplots=(8, 9)):
         [description], by default (8, 9)
     """
 
-    mapinfo = self.ratemaps
+    # mapinfo = self.ratemaps
+    mapinfo = ratemap
     nCells = len(mapinfo["pos"])
 
     def plot_(cell, ax):
         if subplots is None:
             ax.clear()
-        ax.plot(self.x, self.t, color="gray", alpha=0.6)
+        ax.plot(x, t, color="gray", alpha=0.6)
         ax.plot(mapinfo["pos"][cell], mapinfo["spikes"][cell], ".", color="#ff5f5c")
         ax.set_title(
-            " ".join(filter(None, ("Cell", str(cell), self.run_dir.capitalize())))
+            " ".join(filter(None, ("Cell", str(cell), run_dir.capitalize())))
         )
         ax.invert_yaxis()
         ax.set_xlabel("Position (cm)")
