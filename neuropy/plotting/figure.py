3cb976774da72e74d462208a01645bc7c6e51d06
bapungiri <bapun.k.giri@gmail.com>
2021-12-30 12:30:10 -0500

    TimeFrequency renamed to Spectrogram

--- neuropy/plotting/figure.py
+++ neuropy/plotting/figure.py
@@ -83,6 +83,24 @@ class Colormap:
 
         return colmap
 
+    def dynamic4(self):
+        white = 255 * np.ones(80).reshape(20, 4)
+        white = white / 255
+        jet = mpl.cm.get_cmap("jet")
+        greys = mpl.cm.get_cmap("Greys")
+
+        colmap = np.vstack(
+            (
+                ListedColormap(greys(np.linspace(0.5, 0.8, 12))).colors,
+                ListedColormap(jet(np.linspace(0, 1, 30))).colors,
+                ListedColormap(greys(np.linspace(0.5, 0.8, 12)[::-1])).colors,
+            )
+        )
+
+        colmap = ListedColormap(colmap)
+
+        return colmap
+
 
 class Fig:
     labelsize = 8
@@ -98,6 +116,8 @@ class Fig:
             mpl.rcParams["ytick.labelsize"] = 8
             mpl.rcParams["axes.spines.top"] = False
             mpl.rcParams["axes.spines.right"] = False
+            mpl.rcParams["xtick.major.width"] = 2
+            mpl.rcParams["ytick.major.width"] = 2
             mpl.rcParams["axes.prop_cycle"] = cycler(
                 "color",
                 [
@@ -205,6 +225,11 @@ class Fig:
 
         for side in sides:
             ax.spines[side].set_linewidth(lw)
+
+    @staticmethod
+    def center_spines(ax):
+        ax.spines["left"].set_position("zero")
+        ax.spines["bottom"].set_position("zero")
 
 
 def pretty_plot(ax, round_ylim=False):
