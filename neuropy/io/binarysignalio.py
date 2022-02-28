81ea63d92260f740e3d1ee9e27a52b4291e4aea1
bapungiri <bapun.k.giri@gmail.com>
2022-02-28 10:28:30 -0500

    duration slice for Epoch

--- neuropy/io/binarysignalio.py
+++ neuropy/io/binarysignalio.py
@@ -1,7 +1,5 @@
 import numpy as np
-import pandas as pd
-
-from ..core import Signal
+from ..core import Signal, Epoch
 from pathlib import Path
 
 
@@ -27,30 +25,12 @@ class BinarysignalIO:
         )
 
     @property
-    def _dtype(self):
-        return self.dtype
-
-    @property
     def duration(self):
         return self._raw_traces.shape[1] / self.sampling_rate
 
     @property
     def n_frames(self):
         return self._raw_traces.shape[1]
-
-    def infer_start_time(self, stat_modify_time):
-        """Infers the start time based on modify time for file obtained from "stat" command, which should correspond
-        to when the file was saved/recording stopped.
-        :param stat_modify_time: str copied and pasted from running "$ stat filename",
-        e.g. "2021-08-03 11:57:37.224000000"
-        :return:
-        """
-        mod_datetime = pd.to_datetime(
-            stat_modify_time
-        )  # convert modify time to datetime
-        start_time = mod_datetime - pd.to_timedelta(self.duration, unit="sec")
-
-        return start_time
 
     def get_signal(self, channel_indx=None, t_start=None, t_stop=None):
 
@@ -71,13 +51,61 @@ class BinarysignalIO:
         else:
             sig = self._raw_traces[channel_indx, frame_start:frame_stop]
 
+        if sig.ndim == 1:
+            sig = sig.reshape(1, -1)
         return Signal(
-            sig,
-            self.sampling_rate,
-            t_start,
+            traces=sig,
+            sampling_rate=self.sampling_rate,
+            t_start=t_start,
             channel_id=channel_indx,
-            source_file=self.source_file,
         )
+
+    def frame_slice(self, channel_indx=None, frame_start=None, frame_stop=None):
+
+        if isinstance(channel_indx, int):
+            channel_indx = [channel_indx]
+
+        if frame_start is None:
+            frame_start = 0
+
+        if frame_stop is None:
+            frame_stop = self.n_frames
+
+        if channel_indx is None:
+            sig = self._raw_traces[:, frame_start:frame_stop]
+        else:
+            sig = self._raw_traces[channel_indx, frame_start:frame_stop]
+
+        if sig.ndim == 1:
+            sig = sig.reshape(1, -1)
+        return Signal(
+            traces=sig,
+            sampling_rate=self.sampling_rate,
+            t_start=frame_start / self.sampling_rate,
+            channel_id=channel_indx,
+        )
+
+    def get_frames_within_epochs(self, epochs: Epoch, channel_indx, ret_time=False):
+        """Return concatenated frames corresponding to epochs
+
+        Parameters
+        ----------
+        epochs : Epoch
+            start and stop of epochs
+        channel_indx : int/list
+            channels by index location in the binary file
+
+        Returns
+        -------
+        array
+            concatenated frames
+        """
+        epochs_frames = (epochs.as_array() * self.sampling_rate).astype("int")
+        frames = np.concatenate([np.arange(*e) for e in epochs_frames])
+        if ret_time:
+            return self._raw_traces[channel_indx, frames], frames / self.sampling_rate
+        else:
+            return self._raw_traces[channel_indx, frames]
 
     def write_time_slice(self, write_filename, t_start, t_stop):
 
