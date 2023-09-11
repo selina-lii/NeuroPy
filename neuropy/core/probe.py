ee7fc79c5c23221c09aee996a10948994a1f99a2
nkinsky <nkinsky@umich.edu>
2023-09-11 16:34:50 -0400

    enhancement - return power with epochs

--- neuropy/core/probe.py
+++ neuropy/core/probe.py
@@ -102,7 +102,7 @@ class Shank:
     def n_contacts(self):
         return len(self.x)
 
-    def to_dict(self, recurrsively=False):
+    def to_dict(self):
         layout = {
             "x": self.x,
             "y": self.y,
@@ -195,7 +195,7 @@ class Probe:
             shank_df["shank_id"] = (self.n_shanks - 1) * np.ones(shank.n_contacts)
             self._data = self._data.append(shank_df)
 
-    def to_dict(self, recurrsively=False):
+    def to_dict(self):
         return self._data.to_dict()
 
     def to_dataframe(self):
@@ -295,6 +295,26 @@ class ProbeGroup(DataWriter):
             [shank_ids[np.where(channel_ids == _)[0]] for _ in channel_id]
         )
 
+    def get_probe_id_for_channels(self, channel_id):
+        """Get probe ids for the channels.
+
+        Parameters
+        ----------
+        channel_id : array
+            channel_ids, can have repeated values
+
+        Returns
+        -------
+        array
+            probe_ids corresponding to the channels
+        """
+        probe_ids = self.probe_id
+        channel_ids = self.channel_id
+
+        return np.concatenate(
+            [probe_ids[np.where(channel_ids == _)[0]] for _ in channel_id]
+        ).astype("int")
+
     def get_probe(self):
         pass
 
@@ -342,7 +362,7 @@ class ProbeGroup(DataWriter):
 
         # _, counts = np.unique(self.get_channel_ids(), return_counts=True)
 
-    def to_dict(self, recurrsively=False):
+    def to_dict(self):
         return {
             "data": self._data,
             "metadata": self.metadata,
@@ -353,6 +373,12 @@ class ProbeGroup(DataWriter):
         prbgrp = ProbeGroup(metadata=d["metadata"])
         prbgrp._data = d["data"].sort_values(["shank_id", "y"], ascending=[True, False])
         return prbgrp
+
+    @staticmethod
+    def from_file(f):
+        d = DataWriter.from_file(f)
+        if d is not None:
+            return ProbeGroup.from_dict(d)
 
     def to_dataframe(self):
         return pd.DataFrame(self._data)
