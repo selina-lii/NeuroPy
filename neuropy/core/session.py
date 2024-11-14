4f479506882b41449537024cce22728277a29176
kimqi <84738391+kimqi@users.noreply.github.com>
2024-11-14 17:03:42 -0500

    Function for getting recording start time in OE0.6+
    get_ms_start: Grabs milliseconds elapsed since midnight Jan 1st 1970 UTC and then converts datetime to America/Detroit timezones.

--- neuropy/core/session.py
+++ neuropy/core/session.py
@@ -19,22 +19,19 @@ class ProcessData:
         # self.recinfo = NeuroscopeIO(xml_files[0])
         self.recinfo = neuroscopeio.NeuroscopeIO(xml_files[0])
         eegfiles = sorted(basepath.glob("*.eeg"))
-        try:
-            assert len(eegfiles) == 1, "Fewer/more than one .eeg file detected"
-            self.eegfile = binarysignalio.BinarysignalIO(
-                eegfiles[0],
-                n_channels=self.recinfo.n_channels,
-                sampling_rate=self.recinfo.eeg_sampling_rate,
-            )
-        except AssertionError:
-            print("Fewer/more than one .eeg file detected, no eeg file loaded")
+        assert len(eegfiles) == 1, "Fewer/more than one .eeg file detected"
+        self.eegfile = binarysignalio.BinarysignalIO(
+            eegfiles[0],
+            n_channels=self.recinfo.n_channels,
+            sampling_rate=self.recinfo.eeg_sampling_rate,
+        )
         try:
             self.datfile = binarysignalio.BinarysignalIO(
                 eegfiles[0].with_suffix(".dat"),
                 n_channels=self.recinfo.n_channels,
                 sampling_rate=self.recinfo.dat_sampling_rate,
             )
-        except (FileNotFoundError, IndexError):
+        except FileNotFoundError:
             print("No dat file found, not loading")
 
     def __repr__(self) -> str:
